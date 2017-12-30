838246d81fe38cddf024688f899233131a73ffc1
knz21 <cismollco@gmail.com>
2017-12-30 16:00:17 +0900

    suppress warning

--- app/src/main/java/com/knz21/temperature/viewmodel/TemperatureEditViewModel.kt
+++ app/src/main/java/com/knz21/temperature/viewmodel/TemperatureEditViewModel.kt
@@ -5,9 +5,12 @@ import com.knz21.temperature.common.Type
 import com.knz21.temperature.contract.TemperatureContract
 
 class TemperatureEditViewModel(val contract: TemperatureContract, val type: Type) {
+    @Suppress("unused_parameter")
     fun onClickLeft(v: View) {
         contract.minus(type)
     }
+
+    @Suppress("unused_parameter")
     fun onClickRight(v: View) {
         contract.plus(type)
     }
